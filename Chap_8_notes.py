948a7119ba894dfeb9eca2c8124e95f0ecb1640d
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-03-06 20:38:05 -0500

    Add files via upload
    Please ignore the link, unless you want to know more about QBasics.

---
+++ Chap_8_notes.py
@@ -0,0 +1,19 @@
+# Amanda M
+# 3/6/2024
+# Chapter 8: Arrays
+
+# Arrays allow you to store a group of items of the same data type together in memory.
+# PYTHON DOES'NT HAVE a specific ARRAY. We gotta make a list to make "our version" of an array.
+
+GENRE = ["rock_1", "rock_2", "rock_3", "rock_4", "rock_5"]
+for idx in GENRE:           # saying that "call the list that we made"
+    print(idx)              # print the list 
+print("****************")   # A divider for when we target what we look for
+print(GENRE[4])             # The target we're looking for
+
+
+
+
+
+
+# read up on this [link](https://en.wikipedia.org/wiki/QuickBASIC)
