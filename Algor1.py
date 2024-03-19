e2176dbac4eec3b6c63abd05480e9dd501b521f6
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-03-19 17:51:42 -0400

    Add files via upload
    File takes user data, doesn't run/print data back to the user. Due 3/20/2024

---
+++ Algor1.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+s1=[Student("Amanda", 21)]
+
+def adding():
+    fence.append(Student(name, age))
+
+fence = [s1]
+#for idx in [fence]:
+name = input("Input student name: ")
+age = input("Input student age: ")
+
+def print_student(s1):
+    print(s1(f"This student's name is {name} and their age is {age}."))
