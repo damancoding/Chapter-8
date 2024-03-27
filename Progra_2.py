103189ff46d03dbbb121b63a925bec801b560f11
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-03-27 16:32:01 -0400

    Add files via upload

--- Progra_2.py
+++ Progra_2.py
@@ -2,21 +2,34 @@
 # Amanda M
 # Lottery Number Generator
 
-# I'll make a 7 digit lottery number program using array, random generator,  and loops to display the contents of the array.
+# I'll make a 7 digit lottery number program using array ~~random generator~~, and ~~loops to display the contents~~ of the array.
 # array
-gen_ = [ [0, 0, 0],
-         [0, 0, 0, 0], ]
 
 import random
-# random gen 
-LOT_ROLLS = 7
-gen_[0][0] = random.randint(0, 9)
-gen_[0][1] = random.randint(0, 9)
-gen_[0][2] = random.randint(0, 9)
-gen_[1][0] = random.randint(0, 9)
-gen_[1][1] = random.randint(0, 9)
-gen_[1][2] = random.randint(0, 9)
-gen_[1][3] = random.randint(0, 9)
+# random gen
+gen_ = [ [],
+         [] ]
 
 
-print(gen_)
+class celeb_voice():
+    print("Good evening and get ready for tonights lucky numbers!")
+    
+def rolls():
+        i = 0
+        LOT_ROLLS = 7
+        while i < (LOT_ROLLS + 1):
+            gen_ = [[] for j in range(2)]
+            gen_[0] = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9)]
+            gen_[1] = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9), random.randint(0, 9)]
+            gen_.extend([0])
+            gen_.extend([1])
+            
+
+# While loop to display the numbers
+index = 0
+while index < len(gen_):
+    print(gen_[index])
+    index += 1
+
+celeb_voice()
+rolls()
